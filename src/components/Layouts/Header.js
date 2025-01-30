164bfeaa0591748ca93bb4d12034298fd1a4257e
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-30 19:37:52 +0530

    Created header with css

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -6,24 +6,36 @@ import Logo from '../../Assets/Food_Assets/assets/logo/logo.png';
 
 function Header() {
   return (
-    <>
+    <header>
       <Navbar collapseOnSelect expand="lg">
         <Container>
           <Navbar.Brand href="#home">
-            <Link>
+            <Link to="/" className='logo'>
               <img src={Logo} alt='Logo' className='img-fluid'/>
             </Link>
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
+              <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+              <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                <div className='cart'>
+                  <i class="bi bi-cart fs-5"></i>
+                  <em className='roundPoint'>4</em>
+                </div>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </>
+    </header>
   )
 }
 
