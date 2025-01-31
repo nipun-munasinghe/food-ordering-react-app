72309d9996c0b2ff61a8877285b4700754e8ddf2
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-31 14:47:39 +0530

    fixed some issues

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,39 +1,58 @@
-import React, {useState} from 'react';
-import {Container, Nav, Navbar} from 'react-bootstrap';
-import "../../styles/Header.css";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Logo from '../../Assets/Food_Assets/assets/logo/logo.png';
+import "../../styles/Header.css";
 
-function Header() {
-  const [nav, setNav] = useState();
+const Header = () => {
+  const [nav, setNav] = useState(false);
 
-  //scroll navbar
-  
+  // Scroll Navbar
+  const changeValueOnScroll = () => {
+    const scrollValue = document?.documentElement?.scrollTop;
+    scrollValue > 100 ? setNav(true) : setNav(false);
+  };
+
+  window.addEventListener("scroll", changeValueOnScroll);
 
   return (
     <header>
-      <Navbar collapseOnSelect expand="lg">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={`${nav === true ? "sticky" : ""}`}
+      >
         <Container>
           <Navbar.Brand href="#home">
-            <Link to="/" className='logo'>
-              <img src={Logo} alt='Logo' className='img-fluid'/>
+            <Link to="/" className="logo">
+              <img src={Logo} alt="Logo" className="img-fluid" />
             </Link>
           </Navbar.Brand>
-
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/about">About</Nav.Link>
-              <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
-              <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
-              <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
               <Nav.Link as={Link} to="/">
-                <div className='cart'>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/menu">
+                Our Menu
+              </Nav.Link>
+              <Nav.Link as={Link} to="/shop">
+                Shop
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blog">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                <div className="cart">
                   <i class="bi bi-cart fs-5"></i>
-                  <em className='roundPoint'>4</em>
+                  <em className="roundPoint">4</em>
                 </div>
               </Nav.Link>
             </Nav>
@@ -41,7 +60,7 @@ function Header() {
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
