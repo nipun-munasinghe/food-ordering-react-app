cb3e02a7884d7a404e474218e7fe37086e6b6890
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-31 23:53:37 +0530

    Update Section3.js

--- src/pages/Home/Section3.js
+++ src/pages/Home/Section3.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import menu from '../../data/menu.json';
 import Cards from '../../components/Layouts/Cards';
+import { Link } from 'react-router-dom';
 
 function Section3() {
   return (
@@ -25,6 +26,28 @@ function Section3() {
                         // renderRatingIcons={renderRatingIcons}
                     />
                 ))}
+            </Row>
+
+            <Row className='pt-5'>
+                <Col sm={6} lg={5}>
+                    <div className='ads_box ads_img1 mb-5 mb-md-0'>
+                        <h4 className='mb-0'>Get Your Free</h4>
+                        <h5>Cheese Fries</h5>
+                        <Link to='/' className='btn btn_green px-4 rounded-0'>
+                            Learn More
+                        </Link>
+                    </div>
+                </Col>
+
+                <Col sm={6} lg={7}>
+                    <div className='ads_box ads_img'>
+                        <h4 className='mb-0'>Get Your Free</h4>
+                        <h5>Cheese Fries</h5>
+                        <Link to='/' className='btn btn_green px-4 rounded-0'>
+                            Learn More
+                        </Link>
+                    </div>
+                </Col>
             </Row>
         </Container>
     </section>
