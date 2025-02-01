d9e4471623c2390597688c226620cb146353d796
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-02-01 22:48:54 +0530

    Created section6 and set to edit

---
+++ src/pages/Home/Section6.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+
+function Section6() {
+  return (
+    <section className='blog_section'>
+        <Container>
+            <Row>
+                
+            </Row>
+        </Container>
+    </section>
+  )
+}
+
+export default Section6;
